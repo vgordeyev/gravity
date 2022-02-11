b02e58fedcc3f99612c9507f938bbc4a62901f58
volodymyr.gordeyev <v.gordeyev@pragmasoft.com.ua>
2022-02-11 19:36:51 +0200

    initial drawing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as readline from "readline";
+// import * as readline from "readline";
 
 class Coord3d {
 
@@ -39,6 +39,7 @@ class PhysicalObject {
 
     constructor(
         public name: string,
+        public color: string,
         public mass: number,
         public position: Coord3d,
         public speed: Coord3d,
@@ -66,98 +67,187 @@ class PhysicalObject {
 }
 
 
-function updateSystemState(objects: PhysicalObject[]): void {
+function university(system: PhysicalObject[]): () => void {
 
-    for (let obj of objects) {
-        obj.gravities = [];
-    }
+    function updateSystemState(objects: PhysicalObject[]): void { // todo: bind arguments ???
 
-    const G: number = +"6.67e-11";
+        for (let obj of objects) {
+            obj.gravities = [];
+        }
 
-    for (let one of objects) {
-        for (let another of objects) {
-            if (one !== another) {
-                const vector: Coord3d = one.position.vectorTo(another.position);
-                const distance: number = vector.vectorSize();
-                const direction: Coord3d = one.position.direction(another.position);
-                const gravity: number = G * one.mass * another.mass / (distance * distance);
-                const gravityVector: Coord3d = direction.mul(gravity);
-                // const gravityValue = gravityVector.vectorSize();
-                one.gravities.push(gravityVector);
-                // console.log("This: " + JSON.stringify(one));
-                // console.log("Other: " + JSON.stringify(another));
-                // console.log("Distance: " + distance + ", Gravity value: " + gravityValue);
-                // console.log("Vector: " + JSON.stringify(vector));
-                // console.log("Direction: " + JSON.stringify(direction));
-                // console.log("GravityVector: "+ JSON.stringify(gravityVector) + ", gravityValue: " + gravityValue + " \n");
-                // console.log("\n");
+        const G: number = +"6.67e-11";
+
+        for (let one of objects) {
+            for (let another of objects) {
+                if (one !== another) {
+                    const vector: Coord3d = one.position.vectorTo(another.position);
+                    const distance: number = vector.vectorSize();
+                    const direction: Coord3d = one.position.direction(another.position);
+                    const gravity: number = G * one.mass * another.mass / (distance * distance);
+                    const gravityVector: Coord3d = direction.mul(gravity);
+                    // const gravityValue = gravityVector.vectorSize();
+                    one.gravities.push(gravityVector);
+                    // console.log("This: " + JSON.stringify(one));
+                    // console.log("Other: " + JSON.stringify(another));
+                    // console.log("Distance: " + distance + ", Gravity value: " + gravityValue);
+                    // console.log("Vector: " + JSON.stringify(vector));
+                    // console.log("Direction: " + JSON.stringify(direction));
+                    // console.log("GravityVector: "+ JSON.stringify(gravityVector) + ", gravityValue: " + gravityValue + " \n");
+                    // console.log("\n");
+                }
             }
         }
     }
-}
 
-function simulatePeriod(objects: PhysicalObject[], tacts: number, interval: number) {
-    for (let i = 0; i < tacts; i++) {
-        updateSystemState(objects);
-        for (let obj of objects) {
-            obj.move(interval);
+    function simulatePeriod(objects: PhysicalObject[], tacts: number, interval: number) { // todo: bind arguments ???
+        for (let i = 0; i < tacts; i++) {
+            updateSystemState(objects);
+            for (let obj of objects) {
+                obj.move(interval);
+            }
         }
     }
+
+    const INTERVAL: number = 1;
+    let age = 0;
+
+
+    const TACTS: number = 1000;
+    function cycle(): void {
+
+        console.log("\n\nAge: " + age);
+
+        simulatePeriod(system, TACTS, INTERVAL);
+
+        for (let one of system) {
+            console.log("\n");
+            // console.log(JSON.stringify(one));
+            console.log("Name:     " + one.name);
+            console.log("Position:     " + JSON.stringify(one.position));
+            console.log("Speed:        " + JSON.stringify(one.speed));
+            console.log("Acceleration: " + JSON.stringify(one.acceleration()));
+            console.log("Gravity:      " + JSON.stringify(one.gravitySum()));
+        }
+        age += TACTS * INTERVAL;
+    }
+
+    return cycle;
 }
+
+
+function sky(system: PhysicalObject[]): (erase: boolean) => void {
+
+
+    let world: HTMLCanvasElement | null = document.getElementById("space") as HTMLCanvasElement;
+
+    let context: CanvasRenderingContext2D | null = world?.getContext('2d');
+
+    const PADDING: number = 10;
+    const SIDE: number = 700;
+    const UNIVERSITY: Coord3d = new Coord3d(1200, 1200, 1200);
+    const SKY_COLOR: string = 'lightblue';
+    const SCALING: Coord3d = UNIVERSITY.div(SIDE);
+
+    if (context) {
+        context.fillStyle = SKY_COLOR;
+        context.fillRect(PADDING, PADDING, SIDE, SIDE);
+    }
+
+    function drawObject(obj: PhysicalObject, erase?: boolean): void {
+
+        if (!context) {
+            return;
+        }
+
+        const x: number = PADDING + SIDE/2 + obj.position.x / SCALING.x;
+        const y: number = PADDING + SIDE/2 - obj.position.y / SCALING.y;
+
+        context.fillStyle = erase ? SKY_COLOR : obj.color;
+        context.fillRect(erase ? x - 1 : x, erase ? y - 1 : y, erase ? 5 : 3, erase ? 5 : 3);
+        if (erase) {
+            context.fillStyle = "gray";
+            context.fillRect(x, y, 3, 3);
+        }
+    }
+
+    function draw(erase?: boolean): void {
+        if (!context) {
+            return;
+        }
+
+        for (let obj of system) {
+            drawObject(obj, erase);
+        }
+
+    }
+
+    return draw;
+}
+
 
 console.log('Hello there!');
 
-
-const INTERVAL: number = 1;
-let age = 0;
-let system: PhysicalObject[] = [
-    // new PhysicalObject("Big", 1000000, new Coord3d(1000, 500, 0), new Coord3d(0, 0, 0)),
-    new PhysicalObject("Small", 5000, new Coord3d(100, 0, 0), new Coord3d(0, 0.001, 0)),
-    new PhysicalObject("Main", 5000000, new Coord3d(0, 0, 0), new Coord3d(0, 0, 0)),
+let objects: PhysicalObject[] = [
+    new PhysicalObject("Big", "red", 50000, new Coord3d(0, 100, 0), new Coord3d(-0.002, 0, 0)),
+    new PhysicalObject("Small", "green", 5000, new Coord3d(150, 0, 0), new Coord3d(0, 0.001, 0)),
+    new PhysicalObject("Main", "yellow", 5000000, new Coord3d(0, 0, 0), new Coord3d(0, 0, 0)),
 ];
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const epoch = university(objects);
+const view = sky(objects);
 
-function timeMgm(tacts: number) {
-    if (tacts === 0) {
-        updateSystemState(system);
-    }
-    else {
-        simulatePeriod(system, tacts, INTERVAL);
-    }
-    age += tacts;
-    // console.log("\n\n Age: " + age + " intervals by " + INTERVAL + " secs");
+const PAUSE: number = 100;
+/*var intervalId =*/ setInterval(function() {
+    view(true);
+    epoch();
+    view(false);
+}, PAUSE);
 
-    for (let one of system) {
-        console.log("\n");
-        // console.log(JSON.stringify(one));
-        console.log("Position:     " + JSON.stringify(one.position));
-        console.log("Speed:        " + JSON.stringify(one.speed));
-        console.log("Acceleration: " + JSON.stringify(one.acceleration()));
-        console.log("Gravity:      " + JSON.stringify(one.gravitySum()));
-    }
 
-    console.log("\n");
-    rl.question("\n\n Age: " + age + " intervals by " + INTERVAL + " secs " + INTERVAL + " sec): \n",
-        function (answer: string): void {
-                    if (answer === "stop") {
-                        rl.close();
-                        console.log("stopping\n");
-                        process.exit();
-                        // } else if (isNaN(Number(answer))) {
-                        //     timeMgm(Number(tacts));
-                    }
-                    else {
-                        // console.log("\n" + answer);
-                        const nextTacts: number = Number(answer) || tacts;
-                        // console.log("\n" + nextTacts);
-                        timeMgm(nextTacts);
-                    }
-                });
-};
 
-console.log("Start.\n");
-timeMgm(0);
+//
+// var rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+//
+// function timeMgm(tacts: number) {
+//     if (tacts === 0) {
+//         updateSystemState(system);
+//     }
+//     else {
+//         simulatePeriod(system, tacts, INTERVAL);
+//     }
+//     age += tacts;
+//     // console.log("\n\n Age: " + age + " intervals by " + INTERVAL + " secs");
+//
+//     for (let one of system) {
+//         console.log("\n");
+//         // console.log(JSON.stringify(one));
+//         console.log("Position:     " + JSON.stringify(one.position));
+//         console.log("Speed:        " + JSON.stringify(one.speed));
+//         console.log("Acceleration: " + JSON.stringify(one.acceleration()));
+//         console.log("Gravity:      " + JSON.stringify(one.gravitySum()));
+//     }
+//
+//     console.log("\n");
+//     rl.question("\n\n Age: " + age + " intervals by " + INTERVAL + " secs " + INTERVAL + " sec): \n",
+//         function (answer: string): void {
+//                     if (answer === "stop") {
+//                         rl.close();
+//                         console.log("stopping\n");
+//                         process.exit();
+//                         // } else if (isNaN(Number(answer))) {
+//                         //     timeMgm(Number(tacts));
+//                     }
+//                     else {
+//                         // console.log("\n" + answer);
+//                         const nextTacts: number = Number(answer) || tacts;
+//                         // console.log("\n" + nextTacts);
+//                         timeMgm(nextTacts);
+//                     }
+//                 });
+// };
+//
+// console.log("Start.\n");
+// timeMgm(0);
